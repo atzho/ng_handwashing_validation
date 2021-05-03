840dc20907be053b9933c06eb5fee4e9bd43200b
Andrew Z <azhong2002@outlook.com>
2021-05-03 18:29:33 -0400

    Implement time buffer to heuristic detection

--- heuristic_detection.py
+++ heuristic_detection.py
@@ -4,6 +4,8 @@ import math, time
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
+debug = False
+
 def landmark_to_tuple(lm):
     return tuple([round(field[1],3) for field in lm.ListFields()])
 
@@ -28,7 +30,16 @@ def SD(joints):
     diff += math.sqrt((avg[0] - joint[0])**2 + (avg[1] - joint[1])**2 + (avg[2] - joint[2])**2)
   return diff
 
+score = 0
+
 last_wash = time.time()
+
+last_second = math.floor(time.time())
+buffer_size = 30
+buffer_threshold = 15
+dropoff_threshold = 4
+buffer = [0]*buffer_size
+
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
   min_detection_confidence=0.7,
@@ -53,21 +64,37 @@ with mp_hands.Hands(
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     #hold label for 0.5 seconds
-    if time.time() - last_wash > 0.5:
+    if time.time() - last_wash > 0.75:
         label = 'None'
+
+    # Compute label
     if results.multi_hand_landmarks:
-      if len(results.multi_hand_landmarks) == 2:
-        left = landmark_list_to_array(results.multi_hand_landmarks[0])
-        right = landmark_list_to_array(results.multi_hand_landmarks[1])
-        if SD([mean(left), mean(right)])**2 / (SD(left) + SD(right)) < 0.01:
-          label = 'Washing'
-          last_wash = time.time()
-        print(SD([mean(left), mean(right)])**2 / (SD(left) + SD(right)))
-      for hand_landmarks in results.multi_hand_landmarks:
-          mp_drawing.draw_landmarks(
-            image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-    image = cv2.putText(image, label, (0,30), cv2.FONT_HERSHEY_SIMPLEX, 1.1, 2)
+        if len(results.multi_hand_landmarks) == 2:
+            left = landmark_list_to_array(results.multi_hand_landmarks[0])
+            right = landmark_list_to_array(results.multi_hand_landmarks[1])
+            score = SD([mean(left), mean(right)])**2 / (SD(left) + SD(right))
+            if score < 0.01:
+                label = 'Washing'
+                last_wash = time.time()
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+    # Update buffer and compute if finished
+    if sum(buffer[:dropoff_threshold]) == 0:
+        buffer = [0]*buffer_size
+    if time.time() > last_second + 1:
+        last_second = time.time()
+        buffer = ([1] if label == 'Washing' else [0]) + buffer[:-1]
+    if sum(buffer) >= buffer_threshold:
+        image = cv2.putText(image, 'Handwashing complete!', (0,50), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0,255,0), 2)
+        
+    # Add label to image and display
+    image = cv2.putText(image, label, (0,30), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0,255,0), 2)
+    image = cv2.putText(image, repr(sum(buffer)), (150,30), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0,255,0), 2)
     cv2.imshow('MediaPipe Hands', image)
     if cv2.waitKey(5) & 0xFF == 27:
-      break
+        break
+    if debug:
+        print(score, sum(buffer))
 cap.release()
